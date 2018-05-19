364c70129f5c7553d3b84be3aae0862291e9f4a9
Dima <some@mail>
2018-05-19 13:25:01 +0400

    4 реализация сервисов выполняющих работу

---
+++ Domain/Calculator/Concrete/FirstDiscount.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Abstract.Calculator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Calculator.Concrete
+{
+    public class FirstDiscount : IDiscount
+    {
+        public decimal ApplyDiscount(decimal sum)
+        {
+            if(sum <0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (sum > 10 && sum <= 100)
+            {
+                return sum * 0.9m;
+            }
+            else if(sum > 100)
+            {
+                return sum * 0.8m;
+            }
+            else
+            {
+                return sum;
+            }
+        }
+    }
+}
---
+++ Domain/Calculator/Concrete/FirstProductCalculator.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Abstract.Calculator;
+using Domain.DataBaseContextAndEntitiesDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Calculator.Concrete
+{
+    public class FirstProductCalculator:IProductCalculator
+    {
+        IDiscount discount;
+
+        public FirstProductCalculator(IDiscount discount)
+        {
+            this.discount = discount;
+        }
+
+        public decimal TotalSum(List<Products> list)
+        {
+            return list.Sum(x => x.Price * x.Quantity);
+        }
+
+        public decimal TotalSumWithDiscount(decimal sum)
+        {
+            return discount.ApplyDiscount(sum);
+        }
+    }
+}
