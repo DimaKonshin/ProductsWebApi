7b3b8d4d671a773c3fffe4a4feadfbe6866edb83
Dima <some@mail>
2018-05-19 13:30:17 +0400

    5 preparing UnitTest project. add moq

---
+++ UnitTests/1CalculatorTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests._1CalculatorTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
