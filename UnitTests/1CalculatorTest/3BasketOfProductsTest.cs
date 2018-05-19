bb824ead5eeb3abef89398b32f61b1449a3b3547
Dima <some@mail>
2018-05-19 14:24:58 +0400

    12 preparing UnitTest for BasketOfProducts

---
+++ UnitTests/1CalculatorTest/3BasketOfProductsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Collections.Generic;
+using System.Linq;
+using Moq;
+
+using Domain.DataBaseContextAndEntitiesDb;
+using Domain.Calculator.Abstract;
+using Domain.Calculator.Concrete;
+
+namespace UnitTests._3BasketOfProductsTest
+{
+    [TestClass]
+    public class BasketOfProductsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
