2afb551fdaeac63a6c720aa665434d95570a93eb
Dima <some@mail>
2018-05-19 13:07:30 +0400

    2 used pattern Repository. Created access to MSSQL with EF

---
+++ Domain/Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Repository
+{
+    public interface IRepository<TEntity>:IDisposable where TEntity:class
+    {
+        void Create(TEntity item);
+        IQueryable<TEntity> Read();
+        void Update(TEntity item);
+        void Delete(TEntity item);
+    }
+}
---
+++ Domain/Repository/MSSQLEFRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace Domain.Repository
+{
+    class MSSQLEFRepository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        DbContext context;
+        DbSet<TEntity> dbSet;
+
+        public MSSQLEFRepository(DbContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public void Create(TEntity item)
+        {
+            dbSet.Add(item);
+            context.SaveChanges();
+        }
+
+        public IQueryable<TEntity> Read()
+        {
+            return dbSet;
+        }
+
+        public void Update(TEntity item)
+        {
+            context.Entry(item).State = EntityState.Modified;
+            context.SaveChanges();
+        }
+
+        public void Delete(TEntity item)
+        {
+            dbSet.Remove(item);
+            context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
