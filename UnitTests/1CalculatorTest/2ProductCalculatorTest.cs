2c46e30411a179849a4ecdb90463421dde4ce1ea
Dima <some@mail>
2018-05-19 13:57:08 +0400

    8 testing the FirstProductCalculator

--- UnitTests/1CalculatorTest/2ProductCalculatorTest.cs
+++ UnitTests/1CalculatorTest/2ProductCalculatorTest.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using Moq;
 
 using Domain.DataBaseContextAndEntitiesDb;
+using Domain.Calculator.Abstract;
+using Domain.Calculator.Concrete;
 
 namespace UnitTests._2ProductCalculatorTest
 {
@@ -13,17 +15,36 @@ namespace UnitTests._2ProductCalculatorTest
     public class ProductCalculatorTest
     {
         List<Product> list = new List<Product>(){
-            new Product(){Id= 1, Price = 10},
-            new Product(){Id= 2, Price = 20},
-            new Product(){Id= 3, Price = 30},
-            new Product(){Id= 4, Price = 40},
+            new Product(){Id= 1, Price = 10, Quantity=10},
+            new Product(){Id= 2, Price = 20, Quantity = 10},
+            new Product(){Id= 3, Price = 30, Quantity = 10},
+            new Product(){Id= 4, Price = 40, Quantity = 10}
         };
 
 
         [TestMethod]
-        public void TestMethod1()
+        public void Can_FirstProductCalculator_GetSum()
         {
+            Mock<IDiscount> discount = new Mock<IDiscount>();
 
+            IProductCalculator calculator = new FirstProductCalculator(discount.Object);
+
+            decimal sum = calculator.TotalSum(list);
+
+            Assert.AreEqual(1000, sum);
+        }
+
+        [TestMethod]
+        public void Can_FirstProductCalculator_GetSumWithDiscount()
+        {
+            Mock<IDiscount> discount = new Mock<IDiscount>();
+            discount.Setup(x => x.ApplyDiscount(It.IsAny<decimal>())).Returns<decimal>(x => x * 0.9m);
+
+            IProductCalculator calculator = new FirstProductCalculator(discount.Object);
+
+            decimal sum = calculator.TotalSumWithDiscount(1000);
+
+            Assert.AreEqual(900, sum);
         }
     }
 }
