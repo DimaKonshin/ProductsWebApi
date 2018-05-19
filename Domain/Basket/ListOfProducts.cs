18861c932ad68b414c473325b7fd23c4de88a9a8
Dima <some@mail>
2018-05-19 14:10:02 +0400

    10 added functionality to the ListOfProducts

--- Domain/Basket/ListOfProducts.cs
+++ Domain/Basket/ListOfProducts.cs
@@ -4,10 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Domain.DataBaseContextAndEntitiesDb;
+
 namespace Domain.Basket
 {
     public class ListOfProducts
     {
+        List<Product> products = new List<Product>();
 
+        public void CreateUpdate(Product product)
+        {
+            Product temp = products.Where(x => x.Id == product.Id).FirstOrDefault();
+
+            if (temp == null)
+            {
+                products.Add(product);
+            }
+            else
+            {
+                temp.Quantity += product.Quantity;
+            }
+        }
+
+        public List<Product> Read()
+        {
+            return products;
+        }
+
+        public void Delete(Product product)
+        {
+            products.RemoveAll(x => x.Id == product.Id);
+        }
     }
 }
