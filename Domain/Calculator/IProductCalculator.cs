7cfd9902831c25d9a6ee13696d5b42ea7e1669a2
Dima <some@mail>
2018-05-19 13:15:40 +0400

    3 добавлены интерфейсы сервисов выполняющих работу

---
+++ Domain/Calculator/IProductCalculator.cs
@@ -0,0 +1,15 @@
+﻿using Domain.DataBaseContextAndEntitiesDb;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Calculator
+{
+    public interface IProductCalculator
+    {
+        decimal TotalSum(List<Products> list);
+        decimal TotalSumWithDiscount(decimal sum);
+    }
+}
