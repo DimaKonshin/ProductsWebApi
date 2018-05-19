a8caaa802cb879c8154a79bd9cc264d3b48c618a
Dima <some@mail>
2018-05-19 14:14:31 +0400

    11 добавлен сервис корзины

--- Domain/Basket/BasketOfProducts.cs
+++ Domain/Basket/BasketOfProducts.cs
@@ -4,15 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Domain.Calculator.Abstract;
+using Domain.Calculator.Concrete;
+
 namespace Domain.Basket
 {
     public class BasketOfProducts
     {
         ListOfProducts listOfProducts = new ListOfProducts();
 
-        public BasketOfProducts()
-        {
+        IProductCalculator calculator; 
 
+        public BasketOfProducts(IProductCalculator calculator)
+        {
+            this.calculator = calculator;
+        }
+
+        public decimal TotalSum()
+        {
+            return calculator.TotalSum(listOfProducts.Read());
+        }
+
+        public decimal TotalSumWithDiscount()
+        {
+            return calculator.TotalSumWithDiscount(this.TotalSum());
         }
     }
 }
